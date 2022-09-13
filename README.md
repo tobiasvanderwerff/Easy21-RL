2c64b963213bb7055c6398f777a2a252f458eb61
Tobias van der Werff <33268192+tobiasvanderwerff@users.noreply.github.com>
2022-09-13 13:31:34 +0200

    Plot of Monte Carlo value function

---
+++ README.md
@@ -0,0 +1,5 @@
+# Easy 21 - DeepMind RL course
+Implementation of the Easy21 assignment (for more info, see `Easy21.pdf`).
+
+## Monte-Carlo Control
+![monte-carlo](img/mc-500000.png)
--- main.py
+++ main.py
@@ -1,24 +1,46 @@
 import random
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 from step import step
 from util import State, Card, Action, CardColor, CARD_VALS, init_state
 from mc import mc_control
 
 
-n_episodes = 100000
+n_episodes = 500000
 
 state_action_values = mc_control(n_episodes)
 
-print(f"State-action values after {n_episodes} episodes:")
+# print(f"State-action values after {n_episodes} episodes:")
+# for s, av in state_action_values.items():
+#     print(f"dealer card: {s.dealer_first_card.value()}, player sum: {s.player_sum}: {av}")
+
+max_action_val = np.zeros((len(CARD_VALS), 22))
 for s, av in state_action_values.items():
-    print(f"{s}: {av}")
+    dealer_first_card = s.dealer_first_card.value()
+    player_sum = s.player_sum
+    max_action_val[dealer_first_card-1, player_sum-1] = max(av.values())
 
-# total_reward = 0
-# for _ in range(n_episodes):
-#     s = init_state() 
-#     while not s.is_terminal:
-#         a = Action.HIT if s.player_sum < 17 else Action.STICK  # simple policy
-#         s, r = step(s, a)
-#     total_reward += r
+# Plot the action values.
+fig = plt.figure(figsize=(18, 12))
+# x = np.arange(CARD_VALS[0], CARD_VALS[-1] + 1)  # dealer first card
+# y = np.arange(1, 22)  # player sum
+# xv, yv = np.meshgrid(x, y)
+# ax1 = fig.add_subplot(1, 1, 1, projection="3d")
+# ax1.plot_surface(xv, yv, max_action_val)
 
-# print(f"Total reward after {n_episodes} episodes is {total_reward}.")
+plt.imshow(max_action_val)
+for i in range(len(CARD_VALS)):
+    for j in range(21):
+        plt.text(j, i, round(max_action_val[i, j], 2))
+
+plt.xlabel("Player sum")
+plt.ylabel("Dealer showing")
+plt.xticks(np.arange(21), labels=np.arange(1, 22))
+plt.xticks([0, 5, 10, 15, 20])
+plt.yticks(np.arange(len(CARD_VALS)), labels=np.arange(CARD_VALS[0], CARD_VALS[-1] + 1))
+plt.yticks([0, 2, 4, 6, 8])
+plt.title(f"MC value function ({n_episodes} episodes)")
+plt.tight_layout()
+plt.show()
